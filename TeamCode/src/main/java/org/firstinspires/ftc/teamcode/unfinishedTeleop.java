6f8837d5e2bb403ee1ed27432aa2dd97512f4289
DEVLENS <nipun.devlekar@gmail.com>
2023-11-05 15:24:06 -0500

    Add Code for Drone.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/unfinishedTeleop.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class unfinishedTeleop extends LinearOpMode {
+
+    // sets variables for motors
+    private DcMotor driveFL, driveFR, driveBL, driveBR;
+
+    // define drone servo
+    public Servo drone;
+
+    // creates variables for drive inputs from controllers
+    private double forwardBackward, leftRight, rotate;
+
+    @Override
+    public void runOpMode() {
+
+        // function to do init routine
+        initialize();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // runs until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // sets values of variables for gamepad1 (start+a) inputs for driving
+            forwardBackward = gamepad1.left_stick_y;
+            leftRight = gamepad1.left_stick_x;
+            rotate = gamepad1.right_trigger - gamepad1.left_trigger;
+
+            // driving in all directions and rotating
+            driveFL.setPower(forwardBackward + leftRight + rotate);
+            driveFR.setPower(forwardBackward - leftRight - rotate);
+            driveBL.setPower(forwardBackward - leftRight + rotate);
+            driveBR.setPower(forwardBackward + leftRight - rotate);
+
+            if (gamepad1.a) {
+                if (drone.getPosition() < 0.2) drone.setPosition(drone.getPosition() + 0.02);
+            } else {
+                if (drone.getPosition() > 0) drone.setPosition(drone.getPosition() - 0.02);
+            }
+        }
+    }
+
+    // initialization class
+    private void initialize() {
+
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        // maps drive motor variables to hardware configuration names
+        driveFL = hardwareMap.get(DcMotor.class, "motorFL");
+        driveFR = hardwareMap.get(DcMotor.class, "motorFR");
+        driveBL = hardwareMap.get(DcMotor.class, "motorBL");
+        driveBR = hardwareMap.get(DcMotor.class, "motorBR");
+
+        // get drone and set it to 0
+        drone = hardwareMap.get(Servo.class, "drone");
+        drone.setPosition(0);
+
+        // sets left motors to reverse direction so they're going the right way
+        driveFL.setDirection(DcMotor.Direction.REVERSE);
+        driveBL.setDirection(DcMotor.Direction.REVERSE);
+
+        // sets drive motor zero power behavior to brake
+        driveFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        driveBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialization Complete");
+        telemetry.update();
+    }
+}
